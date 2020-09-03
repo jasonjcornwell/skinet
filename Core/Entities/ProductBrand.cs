4b8d09b81b5652c2b4ea0a5a7a8e1eae70513156
Jason <jason.j.cornwell@gmail.com>
2020-09-03 16:09:28 +0200

    Still before migration

---
+++ Core/Entities/ProductBrand.cs
@@ -0,0 +1,10 @@
+// using System.Data.Entity.Core;
+// using System.ComponentModel.DataAnnotations;
+
+namespace Core.Entities
+{
+    public class ProductBrand
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Core/Entities/ProductType.cs
@@ -0,0 +1,10 @@
+// using System.Data.Entity.Core;
+// using System.ComponentModel.DataAnnotations;
+
+namespace Core.Entities
+{
+    public class ProductType
+    {
+        public string Name { get; set; }
+    }
+}
