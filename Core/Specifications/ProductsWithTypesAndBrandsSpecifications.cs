8673ac8e41c7787dfd450d7e4fa26a757dc8a6b2
Jason <jason.j.cornwell@gmail.com>
2020-09-03 22:23:37 +0200

    End of section 4

---
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecifications.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Linq.Expressions;
+using Core.Entities;
+
+namespace Core.Specifications
+{
+    public class ProductsWithTypesAndBrandsSpecifications : BaseSpecification<Product>
+    {
+        public ProductsWithTypesAndBrandsSpecifications()
+        {
+            AddInclude(x => x.ProductBrand);
+            AddInclude(x => x.ProductType);
+        }
+
+        public ProductsWithTypesAndBrandsSpecifications(int id) : base(x => x.Id == id)
+        {
+            AddInclude(x => x.ProductBrand);
+            AddInclude(x => x.ProductType);
+        }
+    }
+}
